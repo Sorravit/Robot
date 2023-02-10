d80fc5e81f3665ed58d42a01b92c208f17fb7cb7
Sorravit Bunjongpean <sorravit@Sorravits-Tiny-MacBook-Pro.local>
2023-02-10 23:39:48 +0700

    Switch back to only auto click for now

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
+import autoClick.AutoClicker
 import notification.LineNotification
 import util.ScreenshotUtil
 import java.io.FileInputStream
@@ -5,14 +6,14 @@ import java.util.*
 
 const val screenShotName="screenShot.png"
 fun main(args: Array<String>) {
-//    AutoClicker().keepScreenAlive()
-
-    val properties = Properties()
-    val screenshotUtil = ScreenshotUtil()
-    properties.load(FileInputStream("config.properties"))
-    val line = LineNotification(properties.getProperty("token"))
-
-    screenshotUtil.takeScreenshot(screenShotName)
-    println(line.send("Current Screenshot", picture = screenShotName).inputStream.bufferedReader().use { it.readText() })
+    AutoClicker().keepScreenAlive()
+//
+//    val properties = Properties()
+//    val screenshotUtil = ScreenshotUtil()
+//    properties.load(FileInputStream("config.properties"))
+//    val line = LineNotification(properties.getProperty("token"))
+//
+//    screenshotUtil.takeScreenshot(screenShotName)
+//    println(line.send("Current Screenshot", picture = screenShotName).inputStream.bufferedReader().use { it.readText() })
 
 }
